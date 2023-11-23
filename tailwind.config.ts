997fc7bddb2595e9f78642e528bb314e657a532d
Wasu <riversea43@gmail.com>
2023-11-23 12:47:05 +0700

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+   './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+  fontFamily: {
+    roboto:["var(--font-roboto)"],
+    poppins:["var(--font-poppins)"],
+    santhai:["var(--font-santhai)"]
+   },
+     
+    },
+  },
+
+
+
+  
+  plugins: [],
+}
+export default config
